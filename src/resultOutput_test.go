57191255ba8b8443a74823ab483cf627a0cf0614
Nick Liu <105009141+liunick-msft@users.noreply.github.com>
2023-02-23 00:51:22 +0000

    Update sample result files

--- src/resultOutput_test.go
+++ src/resultOutput_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 var (
-	inputVariables = &InputType{InterfaceGUID: "\\Device\\NPF_{0217D729-CED0-4D06-9C66-592E032A37A8}", InterfaceAlias: "Ethernet", NativeVlanID: 710, AllVlanIDs: []int{1, 710, 711, 712}, MTUSize: 9214, ETSMaxClass: 8, ETSBWbyPG: "0:48,1:0,2:0,3:50,4:0,5:2,6:0,7:0", PFCMaxClass: 8, PFCPriorityEnabled: "0:0,1:0,2:0,3:1,4:0,5:0,6:0,7:0"}
+	inputVariables = &InputType{InterfaceName: "eth0", InterfaceGUID: "\\Device\\NPF_{0217D729-CED0-4D06-9C66-592E032A37A8}", InterfaceAlias: "Ethernet", NativeVlanID: 710, AllVlanIDs: []int{1, 710, 711, 712}, MTUSize: 9214, ETSMaxClass: 8, ETSBWbyPG: "0:48,1:0,2:0,3:50,4:0,5:2,6:0,7:0", PFCMaxClass: 8, PFCPriorityEnabled: "0:0,1:0,2:0,3:1,4:0,5:0,6:0,7:0"}
 )
 
 func TestResultOutput(t *testing.T) {
@@ -43,11 +43,6 @@ func TestResultOutput(t *testing.T) {
 			got := &OutputType{}
 			pcapFile := filepath.Join(testInputFolder, tc.inputFileName+".pcap")
 			got.resultAnalysis(pcapFile, inputVariables)
-			// Generate Yaml and PDF files for view
-			outputPdfFile := filepath.Join(testOutputFolder, tc.inputFileName+".pdf")
-			got.outputPDFFile(outputPdfFile)
-			outputYamlFile := filepath.Join(testOutputFolder, tc.inputFileName+".yml")
-			got.outputYAMLFile(outputYamlFile)
 			// Parse Golden Yaml to Go Object to compare
 			goldenYamlFile := filepath.Join(testGoldenFolder, tc.inputFileName+".yml")
 			want := parseYamlToGo(goldenYamlFile)
@@ -60,6 +55,13 @@ func TestResultOutput(t *testing.T) {
 			if !reflect.DeepEqual(want.LLDPResult, got.LLDPResult) {
 				t.Errorf("%s - LLDPResult Failed \n want: %v \n got: %v", name, want.LLDPResult, got.LLDPResult)
 			}
+			// Generate Yaml JSON PDF files for view
+			pdfFileName := filepath.Join(testOutputFolder, tc.inputFileName+".pdf")
+			got.outputPDFFile(pdfFileName)
+			yamlFileName := filepath.Join(testOutputFolder, tc.inputFileName+".yml")
+			got.outputYAMLFile(yamlFileName)
+			jsonFileName := filepath.Join(testOutputFolder, tc.inputFileName+".json")
+			got.outputJSONFile(jsonFileName)
 		})
 	}
 }
